ea0c714921338c0a8721a8616b17e100c1f04af0
Miss_Jo <joana.teye@ashesi.edu.gh>
2022-03-23 16:46:55 +0000

    added database

---
+++ errigate.sql
@@ -0,0 +1,139 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 23, 2022 at 04:45 PM
+-- Server version: 5.7.31
+-- PHP Version: 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `errigate`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `beds`
+--
+
+DROP TABLE IF EXISTS `beds`;
+CREATE TABLE IF NOT EXISTS `beds` (
+  `id` int(11) NOT NULL,
+  `farm_id` int(11) DEFAULT NULL,
+  `humidity` varchar(100) DEFAULT NULL,
+  `temperature` varchar(100) DEFAULT NULL,
+  `pH` varchar(100) DEFAULT NULL,
+  `nitrogen` varchar(100) DEFAULT NULL,
+  `phosphorous` varchar(100) DEFAULT NULL,
+  `potassium` varchar(100) DEFAULT NULL,
+  `water_used` varchar(100) DEFAULT NULL,
+  `is_valve_open` varchar(100) DEFAULT NULL,
+  `is_manual` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `farm_id` (`farm_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `farm`
+--
+
+DROP TABLE IF EXISTS `farm`;
+CREATE TABLE IF NOT EXISTS `farm` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `farm_name` varchar(100) DEFAULT NULL,
+  `location` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `farm_weather`
+--
+
+DROP TABLE IF EXISTS `farm_weather`;
+CREATE TABLE IF NOT EXISTS `farm_weather` (
+  `id` int(11) NOT NULL,
+  `farm_id` int(11) DEFAULT NULL,
+  `temperature` varchar(100) DEFAULT NULL,
+  `humidity` varchar(100) DEFAULT NULL,
+  `wind_speed` varchar(100) DEFAULT NULL,
+  `rainfall` varchar(100) DEFAULT NULL,
+  `solar_radiation` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `farm_id` (`farm_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sensor`
+--
+
+DROP TABLE IF EXISTS `sensor`;
+CREATE TABLE IF NOT EXISTS `sensor` (
+  `id` int(11) NOT NULL,
+  `farm_id` int(11) DEFAULT NULL,
+  `sensor_name` varchar(100) DEFAULT NULL,
+  `location` varchar(100) DEFAULT NULL,
+  `type` varchar(100) DEFAULT NULL,
+  `value_recorded` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `farm_id` (`farm_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tanks`
+--
+
+DROP TABLE IF EXISTS `tanks`;
+CREATE TABLE IF NOT EXISTS `tanks` (
+  `id` int(11) NOT NULL,
+  `farm_id` int(11) DEFAULT NULL,
+  `level` varchar(100) DEFAULT NULL,
+  `refill` varchar(100) DEFAULT NULL,
+  `rate` varchar(100) DEFAULT NULL,
+  `is_pump_open` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `farm_id` (`farm_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `is_admin` tinyint(1) DEFAULT NULL,
+  `phone_number` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
